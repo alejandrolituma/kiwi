6f17f1b0b52adab47f2b1e4036969d866800f28d
Alejandro Lituma <alejandro_lituma@hotmail.com>
2017-09-08 04:19:57 +0000

    add student Name to index view

--- app/models/student_profile.rb
+++ app/models/student_profile.rb
@@ -1,3 +1,4 @@
 class StudentProfile < ActiveRecord::Base
-
+  has_many :memberships, as: :member, dependent: :destroy
+  has_many :teams, through: :memberships, source: :joinable, source_type: "Team"
 end
